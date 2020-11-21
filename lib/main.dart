35da00b3cd14d454767fd2882f4f5ce02062dab9
ianvillamia <ianvillamia0123@gmail.com>
2020-11-22 02:05:41 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:elementsadmin/Provider/learningProvider.dart';
 import 'package:elementsadmin/Screens/Dashboard/dashboard.dart';
 import 'package:elementsadmin/Screens/LessonsModule/lessonsModule.dart';
 import 'package:elementsadmin/Screens/Authentication/login.dart';
 import 'package:flutter/material.dart';
 import 'package:elementsadmin/Services/routing.dart';
+import 'package:provider/provider.dart';
 import 'Strings/routes.dart';
 
 void main() {
@@ -13,15 +15,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        onGenerateRoute: FluroRouter.router.generator,
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/',
-        title: 'Elements++ Admin',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          fontFamily: 'Roboto',
-        ),
-        home: Dashboard());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<LearningProvider>(
+            create: (_) => LearningProvider()),
+      ],
+      child: MaterialApp(
+          onGenerateRoute: FluroRouter.router.generator,
+          debugShowCheckedModeBanner: false,
+          initialRoute: '/',
+          title: 'Elements++ Admin',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            fontFamily: 'Roboto',
+          ),
+          home: Dashboard()),
+    );
   }
 }
